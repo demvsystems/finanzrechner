bc701f7ad7bedb1e6a15c3ed761ddd78283145c6
Alex Mihm <mihm.alexander@googlemail.com>
2018-03-20 17:08:18 +0100

    Kapitalstockrechner

---
+++ src/Kapitalstock/KapitalstockRechner.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Finanzrechner\Kapitalstock;
+
+use function Dgame\Ensurance\ensure;
+
+/**
+ * Class KapitalstockRechner
+ * @package Finanzrechner\Kapitalstock
+ */
+final class KapitalstockRechner
+{
+    /**
+     * @var float
+     */
+    private $zinssatz;
+
+    /**
+     * KapitalstockRechner constructor.
+     *
+     * @param float $zinssatz
+     */
+    public function __construct(float $zinssatz)
+    {
+        ensure($zinssatz)->isPositive();
+        $this->zinssatz = $zinssatz;
+    }
+
+    /**
+     * @param float $monatlicheEntnahme
+     * @param int   $laufzeit
+     *
+     * @return float
+     */
+    public function calc(float $monatlicheEntnahme, int $laufzeit): float
+    {
+        ensure($monatlicheEntnahme)->isPositive();
+        ensure($laufzeit)->isPositive();
+
+        $kapitalstock = 0;
+        for ($counter = 1; $counter <= $laufzeit; $counter++) {
+            $kapitalstock = $kapitalstock / (1 + $this->zinssatz);
+            $kapitalstock += $monatlicheEntnahme * 12;
+        }
+
+        return round($kapitalstock, 2);
+    }
+}
---
+++ tests/Kapitalstock/KapitalstockRechnerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Finanzrechner\Tests\Kapitalstock;
+
+use Finanzrechner\Kapitalstock\KapitalstockRechner;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class KapitalstockRechnerTest
+ * @package Finanzrechner\Tests\Kapitalstock
+ */
+final class KapitalstockRechnerTest extends TestCase
+{
+    private $testcases = [
+        [
+            'laufzeit'     => 2,
+            'entnahme'     => 2000,
+            'zinssatz'     => 0.02,
+            'kapitalstock' => 47529.41
+        ],
+        [
+            'laufzeit'     => 23,
+            'entnahme'     => 2000,
+            'zinssatz'     => 0.03,
+            'kapitalstock' => 406486
+        ],
+        [
+            'laufzeit'     => 25,
+            'entnahme'     => 999,
+            'zinssatz'     => 0.02,
+            'kapitalstock' => 238728.14
+        ],
+        [
+            'laufzeit'     => 21,
+            'entnahme'     => 2345,
+            'zinssatz'     => 0.015,
+            'kapitalstock' => 511265.50
+        ]
+    ];
+
+    public function testCalc()
+    {
+        foreach ($this->testcases as $testcase) {
+            $this->assertEquals($testcase['kapitalstock'],
+                                (new KapitalstockRechner($testcase['zinssatz']))->calc($testcase['entnahme'], $testcase['laufzeit']));
+        }
+    }
+}
